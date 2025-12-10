4897736636c6e9eb6cc2f5071e6ef040ba783dab
HocineCh3 <houcinechafai3@gmail.com>
2025-12-11 00:14:10 +0100

    Add files via upload

--- util/IOUtilities.java
+++ util/IOUtilities.java
@@ -31,7 +31,7 @@ import javax.annotation.Nullable;
  * IO tools that depend on JDK only.
  *
  * @author Matthieu Casanova
- * @version $Id: IOUtilities.java 25273 2020-04-19 16:29:30Z kpouer $
+ * @version $Id: IOUtilities.java 25698 2023-11-17 01:31:50Z vampire0 $
  * @since 4.3pre5
  */
 public class IOUtilities
